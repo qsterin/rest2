8964534edf5856ae64321bc3292030363ba80c53
 Eвгений Костерин <qsterin@gmail.com>
2022-10-14 11:32:29 +0300

    fail CI

--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -11,21 +11,26 @@ import static org.hamcrest.Matchers.*;
 class MobileBankApiTestV3 {
     @Test
     void shouldReturnDemoAccounts() {
-      // Given - When - Then
-      // Предусловия
-      given()
-          .baseUri("http://localhost:9999/api/v1")
-      // Выполняемые действия
-      .when()
-          .get("/demo/accounts")
-      // Проверки
-      .then()
-          .statusCode(200)
-          // специализированные проверки - лучше
-          .contentType(ContentType.JSON)
-          .body("", hasSize(3))
-          .body("[0].currency", equalTo("RUB"))
-          .body("[0].balance", greaterThanOrEqualTo(0))
-              .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+        // .contentType(ContentType.JSON)
+        //.body("", hasSize(3))
+        // .contentType(ContentType.JSON)
+        //.body("", hasSize(3))
+        // .contentType(ContentType.JSON)
+        //.body("", hasSize(3))
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("", hasSize(3))
+                .body("[0].currency", equalTo("RUR"))
+                .body("[0].balance", greaterThanOrEqualTo(0))
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
     }
+
+
 }
